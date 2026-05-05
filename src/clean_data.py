40df25d925ba7c92f5dee47d75102b9e1ce4118d
kab0592 <kab0592@gmail.com>
2026-05-05 12:47:23 -0600

    Merged datasets and feature engineering

--- src/clean_data.py
+++ src/clean_data.py
@@ -5,8 +5,8 @@ import pandas as pd
 import numpy as np
 
 pop2019_info = pd.read_csv('data/raw/population2019_data_raw.csv', encoding='latin1')
-pop2019_info.columns = ['Canton', 'Population_2015', 'Population_2019', 'Growth (%)']
-pop2019_info.drop(columns=['Population_2015', 'Growth (%)'],inplace=True)
+pop2019_info.columns = ['canton', 'population_2015', 'population_2019', 'growth_%']
+pop2019_info.drop(columns=['population_2015', 'growth_%'],inplace=True)
 pop2019_info = pop2019_info.iloc[:-5]
 
 new_canton_names = ['Sarapiqui', 'Garabito', 'Los Chiles', 'Carrillo', 'Talamanca',
@@ -15,8 +15,8 @@ new_canton_names = ['Sarapiqui', 'Garabito', 'Los Chiles', 'Carrillo', 'Talamanc
                     'Pococi', 'Barva', 'Puntarenas', 'Santa Barbara', 'Upala', 'Alajuela',
                     'Santa Ana', 'San Rafael', 'Buenos Aires', 'Turrubares', 'Guatuso',
                     'Matina', 'Flores', 'Palmares', 'Canas', 'Golfito', 'San Ramon',
-                    'Corredores', 'Heredia', 'Zarcero', 'Naranjo', 'Valverde Vega',
-                    'San Isidro', 'La Union', 'Leon Cortes Castro', 'Atenas', 'Vasquez de Coronado',
+                    'Corredores', 'Heredia', 'Zarcero', 'Naranjo', 'Sarchi',
+                    'San Isidro', 'La Union', 'Leon Cortes Castro', 'Atenas', 'Vazquez de Coronado',
                     'Montes de Oro', 'San Mateo', 'Nicoya', 'Desamparados', 'Escazu',
                     'Santo Domingo', 'Goicoechea', 'Puriscal', 'Aserri', 'Belen', 'Mora',
                     'El Guarco', 'Tarrazu', 'Curridabat', 'Paraiso', 'Oreamuno', 'Tibas',
@@ -25,34 +25,34 @@ new_canton_names = ['Sarapiqui', 'Garabito', 'Los Chiles', 'Carrillo', 'Talamanc
                     'Limon', 'Montes de Oca', 'Nandayure', 'Jimenez', 'Coto Brus',
                     'Perez Zeledon', 'Turrialba', 'Rio Cuarto', 'Grecia']
 
-pop2019_info['Canton'] = new_canton_names
-pop2019_info['Population_2019'] = pop2019_info['Population_2019'].astype(int)
-#pop2019_info.to_csv('data/cleaned/population2019_clean.csv', index=False)
+pop2019_info['canton'] = new_canton_names
+pop2019_info['population_2019'] = pop2019_info['population_2019'].astype(int)
+pop2019_info.to_csv('data/cleaned/population2019_clean.csv', index=False)
 
 #########################################################################################
 
 pop2022_info = pd.read_csv('data/raw/population2022_data_raw.csv', encoding='latin1')
-pop2022_info.rename(columns={'Cantón': 'Canton'},inplace=True)
-pop2022_info.rename(columns={'2022': 'Population_2022'},inplace=True)
+pop2022_info.rename(columns={'Cantón': 'canton'},inplace=True)
+pop2022_info.rename(columns={'2022': 'population_2022'},inplace=True)
 
 pop2022_info.drop(columns=['1973', '1984', '2000', '2011'],inplace=True)
 pop2022_info.drop(pop2022_info.index[0], inplace=True)
 
-pop2022_info['Canton'] = (pop2022_info['Canton'].str.normalize('NFKD').str.encode(
+pop2022_info['canton'] = (pop2022_info['canton'].str.normalize('NFKD').str.encode(
     'ascii', errors='ignore').str.decode('utf-8'))
 
 pop2022_info = pop2022_info.replace('Leon Cortes', 'Leon Cortes Castro')
-#pop2022_info.to_csv('data/cleaned/population2022_clean.csv', index=False)
+pop2022_info.to_csv('data/cleaned/population2022_clean.csv', index=False)
 
 ##########################################################################################
 
 econ_info = pd.read_csv('data/raw/economic_data_raw.csv',encoding='latin1',thousands=',',
                         na_values=[' -   ', '-', ''], skipinitialspace=True)
-econ_info.columns = ['Year', 'Region', 'Province_code', 'Province', 'Canton_code',
-                    'Canton', 'Added_value', 'Product_tax', 'GDP', 'Exports', 'Imports']
+econ_info.columns = ['year', 'region', 'province_code', 'province', 'canton_code',
+                    'canton', 'added_value', 'product_tax', 'gdp', 'exports', 'imports']
 
-text_columns = ['Region', 'Province_code', 'Province', 'Canton_code', 'Canton']
-num_columns = ['Year', 'Added_value', 'Product_tax', 'GDP', 'Exports', 'Imports']
+text_columns = ['region', 'province_code', 'province', 'canton_code', 'canton']
+num_columns = ['year', 'added_value', 'product_tax', 'gdp', 'exports', 'imports']
 
 econ_info[text_columns] = econ_info[text_columns].apply(lambda x: (x.astype(str).str.strip()
                         .str.normalize('NFKD').str.encode('ascii',errors='ignore')
@@ -65,21 +65,21 @@ for col in num_columns:
 
 econ_info[num_columns] = econ_info[num_columns].fillna(0)
 
-econ_info.drop(econ_info[econ_info['Year'].isin([2020, 2021])].index, inplace=True)
+econ_info.drop(econ_info[econ_info['year'].isin([2020, 2021])].index, inplace=True)
 
 econ_info.replace({'Monteverde': 'Puntarenas', 'Puerto Jimenez': 'Golfito'}, inplace=True)
 
-df_2022 = econ_info[econ_info['Year'] == 2022]
-df_2019 = econ_info[econ_info['Year'] == 2019]
+df_2022 = econ_info[econ_info['year'] == 2022]
+df_2019 = econ_info[econ_info['year'] == 2019]
 
-df_2022_agg = (df_2022.groupby(['Year', 'Canton'], as_index=False)
-    .agg({'Region': 'first','Province_code': 'first','Province': 'first',
-            'Canton_code': 'first', 'Added_value': 'sum','Product_tax': 'sum','GDP': 'sum',
-            'Exports': 'sum','Imports': 'sum'}))
+df_2022_agg = (df_2022.groupby(['year', 'canton'], as_index=False)
+    .agg({'region': 'first','province_code': 'first','province': 'first',
+            'canton_code': 'first', 'added_value': 'sum','product_tax': 'sum','gdp': 'sum',
+            'exports': 'sum','imports': 'sum'}))
 
 econ_info = pd.concat([df_2019, df_2022_agg], ignore_index=True)
 
-econ_info = econ_info.sort_values(['Year', 'Canton'])
+econ_info = econ_info.sort_values(['year', 'canton'])
 
 econ_info.to_csv('data/cleaned/economic_clean.csv', index=False)
 #print(econ_info.head())
--- src/merge_data.py
+++ src/merge_data.py
@@ -1,3 +1,45 @@
 import pandas as pd
 
-print("Merge data script ready")
+pop2019 = pd.read_csv('data/cleaned/population2019_clean.csv')
+pop2022 = pd.read_csv('data/cleaned/population2022_clean.csv')
+econ_data = pd.read_csv('data/cleaned/economic_clean.csv')
+
+econ_data_2019 = econ_data[econ_data['year'] == 2019]
+econ_data_2022 = econ_data[econ_data['year'] == 2022]
+
+merged_2019 = econ_data_2019.merge(pop2019, on='canton')
+merged_2019 = merged_2019.rename(columns={'population_2019': 'population'})
+
+merged_2022 = econ_data_2022.merge(pop2022, on='canton')
+merged_2022 = merged_2022.rename(columns={'population_2022': 'population'})
+
+socioeconomic_cr = pd.concat([merged_2019, merged_2022], ignore_index=True)
+
+metrics = {"gdp_pc": lambda df: df["gdp"] / df["population"], "exports_pc": lambda df: df["exports"] / df["population"], 
+           "imports_pc": lambda df: df["imports"] / df["population"], "trade_balance": lambda df: df["exports"] - df["imports"]}
+
+for name, func in metrics.items():
+    socioeconomic_cr[name] = func(socioeconomic_cr)
+
+socioeconomic_cr.to_csv('data/processed/socioeconomic_merged_complete.csv', index=False)
+
+socioeconomic_cr_2019 = socioeconomic_cr[socioeconomic_cr['year'] == 2019]
+socioeconomic_cr_2022 = socioeconomic_cr[socioeconomic_cr['year'] == 2022]
+
+socioeconomic_analysis = socioeconomic_cr_2022.merge(socioeconomic_cr_2019[['canton','gdp', 'exports', 'imports']], on= 'canton')
+socioeconomic_analysis = socioeconomic_analysis.rename(columns={'gdp_x': 'gdp_2022', 'exports_x': 'exports_2022', 
+                                                       'imports_x': 'imports_2022', 'gdp_y': 'gdp_2019', 'exports_y': 'exports_2019', 
+                                                       'imports_y': 'imports_2019'})
+
+metrics = {"gdp_growth": lambda df: (df["gdp_2022"] - df["gdp_2019"]) / df["gdp_2019"],
+           "exports_growth": lambda df: (df["exports_2022"] - df["gdp_2019"]) / df["gdp_2019"], 
+           "imports_growth": lambda df: (df["imports_2022"] - df["imports_2019"]) / df["imports_2019"]}
+
+for name, func in metrics.items():
+    socioeconomic_analysis[name] = func(socioeconomic_analysis)
+
+socioeconomic_analysis["gdp_growth_cat"] = socioeconomic_analysis["gdp_growth"].apply(lambda x:
+                                                                                      "Strong" if x > 0.04 else ("Moderate" if 0.02 < x < 0.03
+                                                                                        else ("Slow" if 0 < x < 0.02 else "Recession")))
+
+socioeconomic_analysis.to_csv('data/processed/socioeconomic_merged_2022.csv', index=False)
